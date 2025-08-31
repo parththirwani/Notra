ab679c6eb1b7eeda32ac98f7d053d1006e1c6a7e
parththirwani <124462644+parththirwani@users.noreply.github.com>
2025-08-31 16:43:43 +0530

    InMemoryStore

--- src/lib/ai/InMeomeryStore.ts
+++ src/lib/ai/InMeomeryStore.ts
@@ -1,7 +1,7 @@
 import { Message } from "@/types/chat";
-
-export class InMeomeryStore {
-    private static instance: InMeomeryStore;
+const EVICTION_TIME = 5 * 60 * 1000; // 5 minutes
+export class InMemoryStore {
+    private static instance: InMemoryStore;
     private store: Record<
         string,
         {
@@ -9,5 +9,44 @@ export class InMeomeryStore {
             evictionTime: number;
         }
     >;
-    private clock : NodeJS.Timer;
+    private clock : NodeJS.Timeout;
+
+    private constructor(){
+        this.store = {},
+        this.clock = setInterval(()=>{
+            const now = Date.now();
+            Object.entries(this.store).forEach(([key, value])=>{
+                if(now>value.evictionTime){
+                    console.log(`evicting key${key}`);
+                    delete this.store[key]
+                }
+            })
+        }, 60*1000)
+    }
+
+    public destroy(){
+        clearInterval(this.clock);
+    }
+
+    static getInstance(){
+        if(!InMemoryStore.instance){
+            InMemoryStore.instance = new InMemoryStore();
+        }
+        return InMemoryStore.instance
+    }
+
+    add(conversationId: string, message: Message){
+        if(!this.store[conversationId]){
+            this.store[conversationId] = {
+                messages: [],
+                evictionTime: Date.now() + EVICTION_TIME
+            }
+        }
+        this.store[conversationId].messages.push(message);
+        this.store[conversationId].evictionTime = Date.now()+EVICTION_TIME;
+    }
+
+    get(conversationId: string): Message[]{
+        return this.store[conversationId]?.messages ?? []
+    }
 }
